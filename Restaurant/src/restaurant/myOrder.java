77b2541d4125e65b86960861dbef78b4b55e1c08
momoNiti <60070041@kmitl.ac.th>
2018-11-14 02:30:25 +0700

    make money value in Bath

--- Restaurant/src/restaurant/myOrder.java
+++ Restaurant/src/restaurant/myOrder.java
@@ -32,7 +32,49 @@ public class myOrder{
 
     public void setPrice_include_vat(double price_include_vat) {
         double temp = price_include_vat * vat;
-        this.price_include_vat = price_include_vat + temp ;
+        double result = price_include_vat + temp;
+        String[] mystr = String.valueOf(result).split("\\.");
+        for(int i=0; i<mystr.length; i++){
+            System.out.println(mystr[i]);
+        }
+        double decimal = 0;
+//        System.out.println("myste[1] length : " + mystr[1].length());
+        if(mystr[1].length() > 1 && Double.parseDouble(mystr[1]) != 0){
+            char mychar1 = mystr[1].charAt(0);
+            char mychar2 = mystr[1].charAt(1);
+            String decimalString = Character.toString(mychar1) + Character.toString(mychar2);
+            System.out.println("DecimalString : "+decimalString);
+            if(Double.parseDouble(decimalString) >= 87.5){
+                decimal = 1;
+            }
+            else if(Double.parseDouble(decimalString) < 87.5 && Double.parseDouble(decimalString) >= 75){
+                decimal = 0.75;
+            }
+            else if(Double.parseDouble(decimalString) < 75 && Double.parseDouble(decimalString) >= 62.5){
+                decimal = 0.75;
+            }
+            else if(Double.parseDouble(decimalString) < 62.5 && Double.parseDouble(decimalString) >= 50){
+                decimal = 0.50;
+            }
+            else if(Double.parseDouble(decimalString) <50 && Double.parseDouble(decimalString) >= 37.5){
+                decimal = 0.50;
+            }
+            else if(Double.parseDouble(decimalString) <37.5 && Double.parseDouble(decimalString) >= 25){
+                decimal = 0.25;
+            }
+            else if(Double.parseDouble(decimalString) < 25 && Double.parseDouble(decimalString) >= 0){
+                decimal = 0;
+            }
+        }
+        else{
+            decimal = 0;
+        }
+        double output = Double.parseDouble(mystr[0]) + decimal;
+        System.out.println("decimal = : " + decimal);
+        System.out.println("Output =  : " + output);
+        System.out.println("----------------");
+        
+        this.price_include_vat = output;
     }
     public double getPrice_include_vat() {
         return price_include_vat;
--- Restaurant/src/restaurant/myOrderDB.java
+++ Restaurant/src/restaurant/myOrderDB.java
@@ -19,7 +19,6 @@ public class myOrderDB {
     private String user;
     private int orderID;
     private Timestamp date;
-    final double vat = 0.07;
     
     public myOrderDB(int orderID, ArrayList<orderedDB> odb, double priceTotal, double price_include_vat, Timestamp date, String user) {
         this.price_include_vat = price_include_vat;
@@ -29,55 +28,27 @@ public class myOrderDB {
         this.date = date;
         this.odb = odb;
     }
-    public int getOrderID() {
-        return orderID;
-    }
-    public void setOrderID(int orderID) {
-        this.orderID = orderID;
-    }
-    public void addFood(orderedDB odb){
-        this.odb.add(odb);
-    }
-    public void showFood(){
-        for(int i=0; i<odb.size(); i++){
-            System.out.println(odb.get(i).getQuantity() + " " + odb.get(i).getName() + " " + odb.get(i).getPrice_each() + " " + odb.get(i).getPrice());
-        }
+    public ArrayList<orderedDB> getOdb() {
+        return odb;
     }
 
-    public void setPrice_include_vat(double price_include_vat) {
-        double temp = price_include_vat * vat;
-        this.price_include_vat = price_include_vat + temp ;
-    }
     public double getPrice_include_vat() {
-        double temp = priceTotal * vat;
-        price_include_vat = temp + priceTotal;
         return price_include_vat;
     }
-    
+
     public double getPriceTotal() {
         return priceTotal;
     }
 
-    public void setPriceTotal(double priceTotal) {
-        this.priceTotal = priceTotal;
-    }
     public String getUser() {
         return user;
     }
-    public void setUser(String user) {
-        this.user = user;
+
+    public int getOrderID() {
+        return orderID;
     }
-    public ArrayList<orderedDB> getOdb() {
-        return odb;
-    }
-    public void setOdb(ArrayList<orderedDB> odb) {
-        this.odb = odb;
-    }
+
     public Timestamp getDate() {
         return date;
     }
-    public void setDate(Timestamp date) {
-        this.date = date;
-    }
-
 }
